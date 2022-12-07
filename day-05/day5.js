42523f88f0c88364627ba21e1eb5e2b6cb6e3ee0
Ana Schlienger <anaiss@theodo.com>
2022-12-07 09:39:55 -0500

    day 5  - part 1

--- day-05/day5.js
+++ day-05/day5.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const allContents = fs.readFileSync('/Users/schliengeranais/Documents/theodo/advent-of-code-2022/day-05/test.txt', 'utf-8');
+const allContents = fs.readFileSync('/Users/schliengeranais/Documents/theodo/advent-of-code-2022/day-05/input5.txt', 'utf-8');
 const lines = allContents.split(/\r?\n/);
 
 const getItemPriority = (item) => {
@@ -18,17 +18,58 @@ const createStacks = (number) => {
     }
     return stacks;
 }
+
+const fillStacks = (line, nbOfStacks, stacks) => {
+    const splitLine = line.split('');
+    for (let stackIndex = 0; stackIndex < nbOfStacks; stackIndex++) {
+        const crate = splitLine.splice(stackIndex, 3).join('')
+        let stack = stacks[stackIndex];
+        if (crate[2] !== ' ') {
+            if (!stack) stack = [crate];
+            else stack.push(crate);
+            stacks[stackIndex] = stack;
+        }
+    }
+    return stacks;
+}
+
+const moveCrate = (line, stacks) => {
+    const nbCrates = Number(line.slice(5, 7));
+    const origin = Number(line.slice(12, 14)) - 1;
+    const destination = Number(line.slice(17, 19)) - 1;
+    const firstStack = stacks[origin];
+    const destStack = stacks[destination];
+    const crates = firstStack.splice(-nbCrates).reverse();
+    stacks[destination] = destStack.concat(crates);
+}
+
     
 const solvePart1 = () => {
     const firstLine = lines[0];
     const nbOfStacks = getStacksNumber(firstLine);
     const stacks = createStacks();
-    lines.forEach((line) => {
-        if (line.length === 0) return;
-       console.log(line, line.length)
-    });
-    console.log("The sum of the priorities of the item types that appear in both compartments of each rucksack is:");
+    let lineIndex = 0;
+    let line = lines[lineIndex];
+    while (line.indexOf('1') < 0) {
+        fillStacks(line, nbOfStacks, stacks);
+        lineIndex++;
+        line = lines[lineIndex];
+    }
+    for (let index = 0; index < nbOfStacks; index++) {
+        const stack = stacks[index];
+        stack.reverse();
+    }
+    lineIndex += 2;
+    line = lines[lineIndex] 
+    while (!!line) {
+        moveCrate(line, stacks)
+        // lines.forEach((line) => { })
+        lineIndex++;
+        line = lines[lineIndex];
+    };
+    console.log("After the rearrangement procedure completes, the satcks look like that:");
     console.log(stacks);
+    stacks.forEach((stack) => console.log(stack.pop()))
 };
 
 solvePart1();
