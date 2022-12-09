b491e50545ceb927487004f0783adb082408c7d1
Ana Schlienger <anaiss@theodo.com>
2022-12-09 12:21:40 -0500

    day 8 - part 2

--- day-08/day8.js
+++ day-08/day8.js
@@ -5,13 +5,11 @@ const input = '/Users/schliengeranais/Documents/theodo/advent-of-code-2022/day-0
 const allContents = readFileSync(input, 'utf-8');
 
 const lines = allContents.split(/\r?\n/);
+lines.pop()
 
 const rows = lines.length;
 const columns = lines[0].length;
-
-const getColumn = (colIndex) => {
-    return lines.map((line) => Number(line[colIndex]));
-}
+let highestScenicScore = 0;
 
 const isOutside = (row, col) => {
     return (row === rows - 1 || col === columns - 1 || row === 0 || col === 0)
@@ -19,46 +17,54 @@ const isOutside = (row, col) => {
 
 const isVisibleLeft = (row, col, trees) => {
     let isVisible = true;
+    let distance = 0;
     for (let j = col - 1; j >= 0; j--) {
+        distance++;
         if (trees[row][j] >= trees[row][col]) {
             isVisible = false;
             break;
         }
     }
-    return isVisible
+    return { isVisible, distance }
 }
 
 const isVisibleRight = (row, col, trees) => {
     let isVisible = true;
+    let distance = 0;
     for (let j = col + 1; j < columns; j++) {
+        distance++;
         if (trees[row][j] >= trees[row][col]) {
             isVisible = false;
             break;
         }
     }
-    return isVisible
+    return { isVisible, distance }
 }
 
 const isVisibleUp = (row, col, trees) => {
     let isVisible = true;
+    let distance = 0;
     for (let i = row - 1; i >= 0; i--) {
+        distance++;
         if (trees[i][col] >= trees[row][col]) {
             isVisible = false;
             break;
         }
     }
-    return isVisible
+    return { isVisible, distance }
 }
 
 const isVisibleDown = (row, col, trees) => {
     let isVisible = true;
+    let distance = 0;
     for (let i = row + 1; i < rows; i++) {
+        distance++;
         if (trees[i][col] >= trees[row][col]) {
             isVisible = false;
             break;
         }
     }
-    return isVisible
+    return { isVisible, distance }
 }
 
 
@@ -69,22 +75,26 @@ const isTreeVisible = (row, col, trees) => {
     const right = isVisibleRight(row, col, trees);
     const up = isVisibleUp(row, col, trees);
     const down = isVisibleDown(row, col, trees);
-    return left || right || up || down;
+
+    const scenicScore = left.distance * right.distance * up.distance * down.distance;
+    highestScenicScore = Math.max(highestScenicScore, scenicScore);
+
+    return left.isVisible || right.isVisible || up.isVisible || down.isVisible;
 };
     
-const solvePart1 = () => {
+const solve = () => {
     const trees = lines.map((line) => line.split("").map((item) => Number(item)));
     let visibleTrees = 0;
     trees.forEach((row, rowIndex) => {
+        if (row.length === 0) return;
         row.forEach((_, colIndex) => {
             if (isTreeVisible(rowIndex, colIndex, trees)) visibleTrees++;
         })
     })
-    console.log(visibleTrees);
+    console.log('Part 1: the number of visible trees is', visibleTrees);
+    console.log('Part 2: the highest scenic score is', highestScenicScore);
 };
 
-const solvePart2 = () => {
-    console.log("part 2");
-};
 
-solvePart1()
+
+solve()
